b9a9c66b52fa9f3f6d00e1a85156b6c1bd3a869b
Arap <arttu.siren@gmail.com>
2019-03-22 23:09:21 +0200

    add dummy test

---
+++ hydraulic-press/src/test/java/fi/zudoku/hydraulic/HuffmanTreeTest.java
@@ -0,0 +1,12 @@
+package fi.zudoku.hydraulic;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HuffmanTreeTest {
+    
+    @Test
+    public void HuffmanTreeGetsConstructedTheRightWay() {
+        assertEquals(0, 1);
+    }
+}
